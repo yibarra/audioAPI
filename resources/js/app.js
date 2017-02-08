aa615b1c2536f5579d54a06767f311319311ded7
Julian Ibarra <yeissonibarra@gmail.com>
2017-02-08 00:16:56 -0200

    mascara y animation frame

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,19 @@
 (function () {
     var app,
         audioAPI = {
+            analyser: null,
             audioContext: null,
 
             options: {
                 svg: {
                     element: null,
-                    size: 500
+                    size: 600
                 },
                 barsLength: 300,
                 sound: null
             },
+
+            sound: null,
 
             init: function () {
                 app = this;
@@ -19,6 +22,70 @@
                     app.loadMusic();
                 } else {
                     console.log('Error...');
+                }
+            },
+
+
+            barCircleTrackingBackground: function (leftChannel, leftChannelTotal, group, radius) {
+                var angleTotals = Math.PI * 2 / app.options.barsLength,
+                    eachBlock = Math.floor(leftChannelTotal / app.options.barsLength);
+
+                for (var i = 0; i < app.options.barsLength; i++) {
+                    var audioBuffKey = Math.floor(eachBlock * i),
+                        heightElement = leftChannel[audioBuffKey] < 0 ? leftChannel[audioBuffKey] * -1 : leftChannel[audioBuffKey] * 120,
+                        angle = i * angleTotals;
+
+                    heightElement = app.maxMin(10, heightElement, 140);
+
+                    var x = Math.cos(angle) * radius + app.options.svg.size,
+                        y = Math.sin(angle) * radius + app.options.svg.size / 2;
+
+                    app.createBarCircleTracking(3, heightElement, x, y, angle, group, '#8F9499');
+                }
+
+                return group;
+            },
+
+            barCircleTrackingCurrentTime: function (currentTime) {
+                if(!currentTime) return false;
+
+                console.log(currentTime, app.options.sound.duration / app.options.barsLength);
+            },
+
+            createBarCircleTracking: function (widthElement, heightElement, x, y, angle, group, index, color) {
+                if (!widthElement || !heightElement || !angle || !group && typeof x == 'number' && typeof y == 'number') return false;
+
+                var element = app.options.svg.element.rect(widthElement, heightElement),
+                    backgroundColor = typeof color == 'string' && color != undefined ? color : '#222';
+
+                element.attr({fill: backgroundColor});
+
+                element.attr({x: 0, y: 0});
+                element.translate(x, y - heightElement / 2);
+                element.rotate(angle * (180 / Math.PI) + 90);
+                element.attr('height', 0).animate(10 * index).attr('height', heightElement);
+
+                if (typeof group == 'object') {
+                    group.add(element);
+                }
+            },
+
+            createCircleTracking: function (leftChannel, leftChannelTotal) {
+                var radius = (app.options.svg.size / 2);
+
+                var group = app.options.svg.element.group();
+                var masking = app.options.svg.element.circle(radius + 66);
+
+                group.translate(-(app.options.svg.size / 2), 0);
+                group.style({opacity: '.7'});
+
+                masking.attr({'fill': 'none', 'stroke': 'red', 'stroke-width': 40});
+                masking.attr({cx: app.options.svg.size + 1, cy: app.options.svg.size / 2});
+
+                var circleBars = app.barCircleTrackingBackground(leftChannel, leftChannelTotal, group, radius - 100);
+
+                if(circleBars != false) {
+                    group.maskWith(masking);
                 }
             },
 
@@ -47,19 +114,49 @@
                 return URL.createObjectURL(file.files[0]);
             },
 
+            frameLooper: function () {
+                window.requestAnimationFrame(app.frameLooper);
+
+                var fbc_array = new Uint8Array(app.analyser.frequencyBinCount);
+                app.analyser.getByteFrequencyData(fbc_array);
+
+                console.log(fbc_array);
+
+                app.barCircleTrackingCurrentTime(app.options.sound.currentTime);
+            },
+
+            fullTracking: function (buffer) {
+                var leftChannel = buffer.getChannelData(0);
+
+                if (leftChannel.length > 0) {
+                    if(app.options.svg.element != null) {
+                        app.options.svg.element.remove();
+                    }
+
+                    app.options.svg.element = SVG('music').size(app.options.svg.size, app.options.svg.size);
+
+                    //create circle tracking
+                    app.createCircleTracking(leftChannel, leftChannel.length);
+                } else {
+                    return false;
+                }
+            },
+
+
+
             loadMusic: function () {
-                app.options.svg.element = SVG('music').size(app.options.svg.size * 2, app.options.svg.size);
-
-                var element = app.options.svg.element.path('M' + app.options.svg.size / 2 + ', ' + app.options.svg.size / 2 +
-                    ' m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0');
-                element.attr({'fill': 'none', 'stroke': '#8F9499', 'stroke-width': 100});
-
-
                 document.querySelector('input').onchange = function () {
                     app.options.sound = new Audio();
                     app.options.sound.src = app.decodeAudioData(this);
-                };
 
+                    app.sound = app.audioContext.createMediaElementSource(app.options.sound);
+                    app.analyser = app.audioContext.createAnalyser();
+
+                    app.sound.connect(app.analyser);
+                    app.analyser.connect(app.audioContext.destination);
+
+                    app.frameLooper();
+                };
 
                 document.getElementById('play').addEventListener('click', function () {
                     app.playSound();
@@ -68,75 +165,6 @@
                 document.getElementById('stop').addEventListener('click', function () {
                     app.stopSound();
                 });
-            },
-
-            fullTracking: function (buffer) {
-                var leftChannel = buffer.getChannelData(0);
-
-                if (leftChannel.length > 0) {
-                    app.createCircleTracking(leftChannel, leftChannel.length);
-                } else {
-                    return false;
-                }
-            },
-
-            backgroundFullTracking: function () {
-
-            },
-
-            createCircleTracking: function (leftChannel, leftChannelTotal) {
-                var radius = app.options.svg.size / 2 - 20;
-
-                var group = app.options.svg.element.group();
-                var masking = app.options.svg.element.circle(radius);
-
-                masking.attr({'fill': 'none', 'stroke': '#8F9499', 'stroke-width': 100});
-                masking.attr({cx: app.options.svg.size, cy: app.options.svg.size / 2});
-                masking.radius(radius);
-
-                var circleBars = app.barCircleTracking(leftChannel, leftChannelTotal, group, radius);
-
-                if(circleBars != false) {
-                    group.maskWith(masking);
-                }
-            },
-
-            createBarCircleTracking: function (widthElement, heightElement, x, y, angle, group, index, color) {
-                if (!widthElement || !heightElement || !angle || !group && typeof x == 'number' && typeof y == 'number') return false;
-
-                var element = app.options.svg.element.rect(widthElement, heightElement),
-                    backgroundColor = typeof color == 'string' && color != undefined ? color : '#222';
-
-                element.attr({fill: backgroundColor});
-
-                element.attr({x: 0, y: 0});
-                element.translate(x, y - heightElement / 2);
-                element.rotate(angle * (180 / Math.PI) + 90);
-                element.attr('height', 0).animate(10 * index).attr('height', heightElement);
-
-                if (typeof group == 'object') {
-                    group.add(element);
-                }
-            },
-
-            barCircleTracking: function (leftChannel, leftChannelTotal, group, radius) {
-                var angleTotals = Math.PI * 2 / app.options.barsLength,
-                    eachBlock = Math.floor(leftChannelTotal / app.options.barsLength);
-
-                for (var i = 0; i < app.options.barsLength; i++) {
-                    var audioBuffKey = Math.floor(eachBlock * i),
-                        heightElement = leftChannel[audioBuffKey] < 0 ? leftChannel[audioBuffKey] * -1 : leftChannel[audioBuffKey] * 200,
-                        angle = i * angleTotals;
-
-                    heightElement = app.maxMin(10, heightElement, 140);
-
-                    var x = Math.cos(angle) * radius + app.options.svg.size,
-                        y = Math.sin(angle) * radius + app.options.svg.size / 2;
-
-                    app.createBarCircleTracking(3, heightElement, x, y, angle, group, '#8F9499');
-                }
-
-                return group;
             },
 
 
@@ -167,11 +195,15 @@
                     app.options.sound.pause();
             },
 
+            /**
+             * context audio api
+             * @returns {boolean}
+             */
             validateAudioAPI: function () {
                 window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;
 
                 try {
-                    app.audioContext = new webkitAudioContext();
+                    app.audioContext = new AudioContext();
                     return true;
                 } catch (e) {
                     console.log('!Your browser does not support AudioContext', e);
